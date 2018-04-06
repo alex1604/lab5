05a205a12e3218aabd811c754e8fac813c53d7c0
Alejandro <92.alejandrogarcia@gmail.com>
2018-04-07 01:35:33 +0200

    add first component, it works but it is not finished. Need to figure out a way to reuse it with several recipees instead of only one

--- src/components/App.vue
+++ src/components/App.vue
@@ -10,20 +10,77 @@
       <h1>Welcome to iFood</h1>
     </div>
     <div class="bottom">
-      To get started, edit <code>./src/components/App.vue</code> and save to reload.<br/>
-      <span class="fade">
-        Checkout <code>./README.md</code> for more usages.
-      </span>
+      Get started by adding your first recipee<br/>
+
+      <show-recipees v-bind:name="recipees[0].name" v-on:my-event="shoppingList"
+      v-on:event-hide="hideShoppingList"
+      v-bind:description="recipees[0].description" v-bind:preparationTime="recipees[0].preparationTime"
+      v-bind:showShoppingList="showOrNot" v-bind:ingredientList="myIngredients"/>
     </div>
   </div>
 </template>
-
+<script src="../../graf/fontawesome-all.js"></script>
 <script>
-  export default {
-    name: 'app'
-  }
+import ShowRecipees from "./ShowAllRecipees.vue"
+export default {
+  name: "app",
+  components: {
+    "show-recipees": ShowRecipees
+  },
+  data: function() {
+    return {
+      recipees: [
+        {
+          name: "Spanish Tortilla",
+          ingredients: [
+            {
+              name: "potato",
+              quantity: "1kg"
+            },
+            {
+              name: "egg",
+              quantity: "8"
+            },
+            {
+              name: "olive oil",
+              quantity: "3dl"
+            },
+            {
+              name: "salt",
+              quantity: "5g"
+            }
+          ],
+          description:
+            "Wash and peel all the potatos before slicing them. The slices should be around 5mm. After this, add a spoon of olive oil and steer. Prepare the frier/pan and when it's ready start frying the potato slices. In the mean time you can start opening the eggs and blend them with a fork or any manual blending tool. Once all the slices are fried, throw the egg mix into the big bowl where you've previously thrown the potato slices. All the slices should be covered by the egg mix. If so, add the salt and throw in the mix into a very hot pan filled with around 5mm/1cm of olive oil. Turn down the temperature to 4/10 and heck the tortilla every 3-4 mins. When the down side is turning gold, turn the torilla upside down and remove when done.",
+          preparationTime: "1.5"
+        }
+      ],
+      thisingredient: Number,
+      showOrNot: false,
+      myIngredients: ''
+    };
+  },
+  methods: {
+    shoppingList: function(event){
+      let currentname = event.target.name;
+      for (let index in this.recipees) {
+        if (this.recipees[index].name = currentname){
+          let ingredients = this.recipees[index].ingredients
+          for(let ingredient in ingredients){
+            this.myIngredients += '- ' + ingredients[ingredient].name + ' (' + ingredients[ingredient].quantity + ')'
+          }
+          this.showOrNot = true
+        }
+      }
+    },
+    hideShoppingList: function(event){
+      this.showOrNot = false
+      this.myIngredients = ''
+    }
+  },
+}
 
-  /************************************
+/************************************
   RECIPEE OBJEKT:
   {
     name: 'spanish tortilla',
@@ -49,7 +106,6 @@
     preparationTime: ''
   }
   */
-
 </script>
 
 <!-- CSS libraries -->
@@ -57,53 +113,59 @@
 
 <!-- Global CSS -->
 <style>
-  code {
-    font-family: Menlo, Monaco, Lucida Console, Liberation Mono, DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace, serif;
-    font-size: 0.9em;
-    white-space: pre-wrap;
-    color: #2c3e50;
-  }
+code {
+  font-family: Menlo, Monaco, Lucida Console, Liberation Mono, DejaVu Sans Mono,
+    Bitstream Vera Sans Mono, Courier New, monospace, serif;
+  font-size: 0.9em;
+  white-space: pre-wrap;
+  color: #2c3e50;
+}
 
-  code::before, code::after {
-    content: '`';
-  }
+code::before,
+code::after {
+  content: "`";
+}
 </style>
 
 <!-- Scoped component css -->
 <!-- It only affect current component -->
 <style scoped>
-  #app {
-    text-align: center;
-  }
+#app {
+  text-align: center;
+}
 
-  #app h1 {
-    color: #2c3e50;
-    font-weight: 300;
-    margin: 0;
-  }
+#app h1 {
+  color: #2c3e50;
+  font-weight: 300;
+  margin: 0;
+}
 
-  .banner {
-    height: 200px;
-    background-color: #f6f6f6;
-    padding: 50px 10px;
-  }
+.banner {
+  height: 200px;
+  background-color: #f6f6f6;
+  padding: 50px 10px;
+}
 
-  .bottom {
-    padding: 80px 10px;
-    font-size: 24px;
-    font-weight: 300;
-  }
+.bottom {
+  padding: 80px 10px;
+  font-size: 24px;
+  font-weight: 300;
+}
 
-  .fade {
-    font-size: 14px;
-  }
+.fade {
+  font-size: 14px;
+}
 
-  .logo {
-    animation: spin 6s 1s infinite linear
-  }
+.logo {
+  animation: spin 6s 1s infinite linear;
+}
 
-  @keyframes spin {
-    from {transform:rotate(0deg);}
-    to {transform:rotate(360deg);}
+@keyframes spin {
+  from {
+    transform: rotate(0deg);
   }
+  to {
+    transform: rotate(360deg);
+  }
+}
 </style>
---
+++ src/components/ShowAllRecipees.vue
@@ -0,0 +1,71 @@
+<template>
+    <div class="awesomeRecipee">
+        <h3 class="recipeeName">{{name}}</h3>
+        <div class="recipeeDescription">Instructions: {{description}}</div>
+        <div class="recipeeTime">{{preparationTime}}h</div>
+        <button class="showShoppingList" v-bind:name="name" v-on:click="$emit('my-event', $event)">What you need</button>
+        <div class="shoppingList" v-bind:class="{'show': showShoppingList}"><div class="close" v-on:click="$emit('event-hide', $event)"><i class="fas fa-times">X</i></div><div class="list">{{ingredientList}}</div></div>
+    </div>
+</template>
+<script src="../../graf/fontawesome-all.js"></script>
+<script>
+
+export default {
+    showShoppingList: '',
+    props: ['name','showShoppingList', 'currentIngredient','ingredientList', 'ingredient', 'quantity', 'description', 'preparationTime']
+}
+
+</script>
+<style scoped>
+    .show{
+        display:flex !important;
+        background-color: rgb(130, 196, 192);
+        width: 50%;
+        min-height: 300px;
+        height: auto;
+        position:absolute;
+        top:50%;
+        left:50%;
+        transform:translate(-50%);
+        z-index:3;
+        flex-direction: column;
+        justify-content:space-around;
+    }
+    .awesomeRecipee{
+        display:flex;
+        flex-direction:column;
+        justify-content: flex-start;
+        font-family:'Ubuntu', sans-serif;
+        margin: 5%;
+    }
+    .recipeeName{
+        align-self:flex-start;
+    }
+    .recipeeIngredients{
+        display:flex;
+        flex-direction:row;
+        justify-content: flex-start;
+        font-family: serif !important;
+    }
+    .eachIngredient{
+        font-size: 16px;
+    }
+    .recipeeDescription{
+        font-family: serif !important;
+        text-align:justify;
+    }
+    .recipeeTime{
+        margin-right:3%;
+        align-self: flex-end;
+    }
+    .shoppingList{
+        display:none;
+    }
+    .close{
+        align-self:flex-end;
+        margin-right:3%;
+    }
+    .close:hover{
+        cursor:pointer;
+    }
+</style>
