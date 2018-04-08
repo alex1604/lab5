67f1f66f623659795c74acf45650615c8fc71581
unknown <annafaix@gmail.com>
2018-04-08 21:26:49 +0200

    changes in style

--- src/components/App.vue
+++ src/components/App.vue
@@ -10,17 +10,19 @@
       <h1>Welcome to iFood</h1>
     </div>
     <div class="bottom">
-      Get started by adding your first recipee<br/>
+      <h2>Get started by adding your first recipee!</h2>
 
       <show-recipees v-for="currentRecipee in recipees" v-bind:key="currentRecipee"
-      v-bind:name="currentRecipee.name" 
+      v-bind:name="currentRecipee.name"
       v-on:my-event="shoppingList"
       v-on:event-hide="hideShoppingList"
       v-bind:description="currentRecipee.description" v-bind:preparationTime="currentRecipee.preparationTime"
       v-bind:showShoppingList="showOrNot" v-bind:ingredientList="myIngredients"/>
     </div>
   </div>
+
 </template>
+
 <script src="../../graf/fontawesome-all.js"></script>
 <script>
 import ShowRecipees from "./ShowAllRecipees.vue"
@@ -75,7 +77,71 @@ export default {
           description:
             "Crack the eggs and blend them with a fork or any manual blending tool. Add the oil to the pan and throw the egg mix when the oil is warm enough. Turn down the temperature to 4/10. When the down side is turning gold, fold it in by its half and leave on the pan for an extra minute.",
           preparationTime: "0.25"
-        }
+        },
+        {
+          name: "Swedish 'kladdkaka'",
+          ingredients: [
+            {
+              name: "butter",
+              quantity: "100 g"
+            },
+            {
+              name: "egg",
+              quantity: "2"
+            },
+            {
+              name:"plain flour",
+              quantity:"150g"
+            },
+            {
+              name:"cocoa powder",
+              quantity:"4 teaspoon"
+            },
+            {
+              name:"vanilla sugar",
+              quantity:"1 tbsp"
+            },
+            {
+              name: "sugar",
+              quantity: "200 g"
+            },
+            {
+              name: "salt",
+              quantity: "1 pince"
+            }
+          ],
+          description:
+            "Melt the butter and leave to cool slightly. Whisk the egg and sugar together until the mixture is light, fluffy and pale. Weigh all the dry ingredients and sift them into the egg and sugar mixture. Fold in until everything is incorporated. Fold in the melted butter until you are left with a smooth chocolate mixture. Pour into a lined cake tin. This recipe fits a normal tin. The cake will not rise, but it will puff up slightly during baking. Bake at 180°C for around 20 minutes. The exact time can vary, so keep an eye on the cake. A perfect kladdkaka is very soft in the middle, but not runny once it has cooled – but almost runny. If you press down gently on the cake whilst its baking, the crust should need a bit of pressure to crack. When this happens, the cake is done. Leave to cool in the tin for at least an hour.",
+          preparationTime: "0.35"
+        },
+        {
+          name: "Slovak gnocchi with bryndza-cheese",
+          ingredients: [
+            {
+              name: "potato",
+              quantity: "1 kg"
+            },
+            {
+              name: "flour",
+              quantity: "400 g"
+            },
+            {
+              name:"bryndza - cheese",
+              quantity:"300 g"
+            },
+            {
+              name:"smoked bacon",
+              quantity: "100 g"
+            },
+            {
+              name: "salt",
+              quantity: "1 pince"
+            },
+          ],
+          description:
+            "Peel potatos, wash and shred them, add salt. Add flour to achive right consistency. Moisten the chopping board, put the part of the dough on it, use a knife to drop a little bit of the dough into the boiling salty water. When gnocchi (in slovak called halušky) are done they will float to the top. Pick them out, add bryndza-cheese and put fried bacon on top.",
+          preparationTime: "0.30"
+        },
       ],
       thisingredient: Number,
       showOrNot: false,
@@ -90,6 +156,7 @@ export default {
         if (this.recipees[index].name == currentname){
           let ingredients = this.recipees[index].ingredients
           for(let ingredient in ingredients){
+
             this.myIngredients += '- ' + ingredients[ingredient].name + ' (' + ingredients[ingredient].quantity + ')'
           }
           this.showOrNot = true
@@ -99,7 +166,8 @@ export default {
     hideShoppingList: function(event){
       this.showOrNot = false
       this.myIngredients = ''
-    }
+    },
+
   },
 }
 
@@ -173,6 +241,7 @@ code::after {
   padding: 80px 10px;
   font-size: 24px;
   font-weight: 300;
+  background-color: #F5E1DA ;
 }
 
 .fade {
--- src/components/ShowAllRecipees.vue
+++ src/components/ShowAllRecipees.vue
@@ -2,11 +2,20 @@
     <div class="awesomeRecipee">
         <h3 class="recipeeName">{{name}}</h3>
         <div class="recipeeDescription">Instructions: {{description}}</div>
-        <div class="recipeeTime">{{preparationTime}}h</div>
+        <div class="recipeeTime">
+          <img src="../../img/clock.svg" alt="clock">
+          <span>{{preparationTime}}h</span>
+        </div>
         <button class="showShoppingList" v-bind:name="name" v-on:click="$emit('my-event', $event)">What you need</button>
-        <div class="shoppingList" v-bind:class="{'show': showShoppingList}"><div class="close" v-on:click="$emit('event-hide', $event)"><i class="fas fa-times">X</i></div><div class="list">{{ingredientList}}</div></div>
+        <div class="shoppingList" v-bind:class="{'show': showShoppingList}">
+          <div class="close" v-on:click="$emit('event-hide', $event)">
+            <span>x</span>
+          </div>
+        <div class="list">{{ingredientList}}</div>
+      </div>
     </div>
 </template>
+
 <script src="../../graf/fontawesome-all.js"></script>
 <script>
 
@@ -23,23 +32,31 @@ export default {
         width: 50%;
         min-height: 300px;
         height: auto;
-        position:absolute;
-        top:50%;
+        position:fixed;
+        top:30%;
         left:50%;
         transform:translate(-50%);
         z-index:3;
         flex-direction: column;
-        justify-content:space-around;
+        justify-content:center;
     }
+
     .awesomeRecipee{
         display:flex;
         flex-direction:column;
         justify-content: flex-start;
         font-family:'Ubuntu', sans-serif;
         margin: 5%;
+        padding: 4%;
+        background-color: #F1F1F1;
+        -webkit-box-shadow: 10px 10px 43px 0px rgba(0,0,0,0.53);
+        -moz-box-shadow: 10px 10px 43px 0px rgba(0,0,0,0.53);
+        box-shadow: 10px 10px 43px 0px rgba(0,0,0,0.53);
+        border-radius: 5px;
     }
     .recipeeName{
         align-self:flex-start;
+        color:#476269;
     }
     .recipeeIngredients{
         display:flex;
@@ -55,17 +72,45 @@ export default {
         text-align:justify;
     }
     .recipeeTime{
-        margin-right:3%;
+        /* margin-right:3%; */
         align-self: flex-end;
+        justify-content: space-around;
+        align-items: center;
+        display: flex;
+        max-width:110px;
+    }
+    .recipeeTime img{
+      height: 20%;
+      width: 20%;
+
     }
     .shoppingList{
         display:none;
+
+    }
+    .showShoppingList{
+      background-color:#40A798;
+      border: none;
+      border-radius:10px;
+      color:#F1F1F1;
+      cursor: help;
+      padding:10px;
+      max-width: 200px;
     }
     .close{
-        align-self:flex-end;
-        margin-right:3%;
+      align-self:flex-end;
+      margin-right:3%;
+
+    }
+    .close span{
+      font-style: normal;
+      font-weight: bold;
+      position: fixed;
+      top: 10px;
+      right: 10px;
+
     }
     .close:hover{
-        cursor:pointer;
+      cursor:pointer;
     }
 </style>
